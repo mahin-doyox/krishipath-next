5b3d6c66f24b53e95b59b54a17b70d8cf8fe6eff
mahin-doyox <muntasirlahiri@gmail.com>
2026-05-17 23:10:40 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: false,   // <-- নতুন লাইন
+  turbopack: false,
   images: {
     remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co.com',
+      },
       {
         protocol: 'https',
         hostname: 'uskcutjjmtcbgezcfbfm.supabase.co',
       },
     ],
+    formats: ['image/webp', 'image/avif'], // আধুনিক ফরম্যাট ব্যবহার করবে, ছবির সাইজ অনেক কমাবে
   },
 };
 export default nextConfig;

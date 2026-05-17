a674e47a67cc004ffb12bdf3312910343f0d01f3
mahin-doyox <muntasirlahiri@gmail.com>
2026-05-17 22:51:13 +0600

    Update route.js

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,32 +1,35 @@
-export async function GET() {
+export async function GET(request) {
+  // সবসময় www ছাড়া URL দেখাবে
+  const host = 'https://krishipath.com';
+
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
-    <loc>https://krishipath.com/</loc>
+    <loc>${host}/</loc>
     <lastmod>2026-05-17</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
-    <loc>https://krishipath.com/blog</loc>
+    <loc>${host}/blog</loc>
     <lastmod>2026-05-17</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://krishipath.com/forum</loc>
+    <loc>${host}/forum</loc>
     <lastmod>2026-05-17</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>https://krishipath.com/bazar</loc>
+    <loc>${host}/bazar</loc>
     <lastmod>2026-05-17</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
-    <loc>https://krishipath.com/prices</loc>
+    <loc>${host}/prices</loc>
     <lastmod>2026-05-17</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>

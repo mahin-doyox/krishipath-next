6ad328ae9c69bfc28e80019d4614e53aac3654e3
mahin-doyox <muntasirlahiri@gmail.com>
2026-05-31 08:18:29 +0600

    Update route.js

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -1,38 +1,62 @@
-export async function GET(request) {
-  // সবসময় www ছাড়া URL দেখাবে
-  const host = 'https://krishipath.com';
+export async function GET() {
+  const host = 'https://www.krishipath.com';
+  const today = new Date().toISOString().split('T')[0]; // YYYY-MM-DD
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>${host}/</loc>
-    <lastmod>2026-05-17</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>1.0</priority>
   </url>
   <url>
+    <loc>${host}/auth?mode=login</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
+  </url>
+  <url>
+    <loc>${host}/auth?mode=register</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.5</priority>
+  </url>
+  <url>
+    <loc>${host}/bazar</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
     <loc>${host}/blog</loc>
-    <lastmod>2026-05-17</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
     <loc>${host}/forum</loc>
-    <lastmod>2026-05-17</lastmod>
+    <lastmod>${today}</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.8</priority>
   </url>
   <url>
-    <loc>${host}/bazar</loc>
-    <lastmod>2026-05-17</lastmod>
+    <loc>${host}/prices</loc>
+    <lastmod>${today}</lastmod>
     <changefreq>daily</changefreq>
     <priority>0.9</priority>
   </url>
   <url>
-    <loc>${host}/prices</loc>
-    <lastmod>2026-05-17</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>0.9</priority>
+    <loc>${host}/crop-disease</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>${host}/crop-chat</loc>
+    <lastmod>${today}</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
   </url>
 </urlset>`;
 

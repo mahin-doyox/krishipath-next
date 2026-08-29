deb3aa176280402a26b504bfc2c6cb9758ae7b39
mahin-doyox <muntasirlahiri@gmail.com>
2026-08-29 15:42:22 +0600

    Update BlogCardSkeleton.js

--- src/components/BlogCardSkeleton.js
+++ src/components/BlogCardSkeleton.js
@@ -1,9 +1,19 @@
 export default function BlogCardSkeleton() {
   return (
     <div className="feature-card skeleton-card">
-      <div className="skeleton" style={{ width: '80%', height: '1.5rem', marginBottom: '0.5rem' }}></div>
-      <div className="skeleton" style={{ width: '60%', height: '1rem', marginBottom: '1rem' }}></div>
-      <div className="skeleton" style={{ width: '100%', height: '3rem' }}></div>
+      {/* ক্যাটাগরি */}
+      <div className="skeleton" style={{ width: '30%', height: '0.8rem', marginBottom: '0.8rem', borderRadius: '20px' }}></div>
+      {/* শিরোনাম */}
+      <div className="skeleton" style={{ width: '85%', height: '1.4rem', marginBottom: '0.6rem' }}></div>
+      <div className="skeleton" style={{ width: '70%', height: '1.4rem', marginBottom: '1rem' }}></div>
+      {/* মেটা তথ্য */}
+      <div className="skeleton" style={{ width: '50%', height: '0.9rem', marginBottom: '1.2rem' }}></div>
+      {/* কন্টেন্ট লাইন */}
+      <div className="skeleton" style={{ width: '100%', height: '0.9rem', marginBottom: '0.5rem' }}></div>
+      <div className="skeleton" style={{ width: '100%', height: '0.9rem', marginBottom: '0.5rem' }}></div>
+      <div className="skeleton" style={{ width: '80%', height: '0.9rem', marginBottom: '1.2rem' }}></div>
+      {/* লাইক বাটন */}
+      <div className="skeleton" style={{ width: '60px', height: '2rem', borderRadius: '20px' }}></div>
     </div>
   );
 }
